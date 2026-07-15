b1e986f10ee51d65b1234a5dd918297ef8366d41
bin <yu200141526@163.com>
2026-07-15 14:23:22 +0800

    V0.5版本

--- main/main.h
+++ main/main.h
@@ -3,9 +3,9 @@
 
 #include "global_define.h"
 
-#define VERSION        "v0.4"
+#define VERSION        "v0.5"
 #define VERSION_MAJOR  0
-#define VERSION_MINOR  4
+#define VERSION_MINOR  5
 
 
 #endif /*__MAIN_H__*/
