2096450589e7798ab582919bb770b57eecdd5c74
bin <yu200141526@163.com>
2026-07-17 16:56:14 +0800

    修改版本号

--- main/main.h
+++ main/main.h
@@ -3,9 +3,9 @@
 
 #include "global_define.h"
 
-#define VERSION        "v0.5"
+#define VERSION        "v0.6"
 #define VERSION_MAJOR  0
-#define VERSION_MINOR  5
+#define VERSION_MINOR  6
 
 
 #endif /*__MAIN_H__*/
