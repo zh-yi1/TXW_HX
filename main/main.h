cbb2d6e91aed00dbebe5d4632a0763399c6d6b98
bin <yu200141526@163.com>
2026-07-21 15:06:43 +0800

    V0.7版本

--- main/main.h
+++ main/main.h
@@ -3,9 +3,9 @@
 
 #include "global_define.h"
 
-#define VERSION        "v0.6"
+#define VERSION        "v0.7"
 #define VERSION_MAJOR  0
-#define VERSION_MINOR  6
+#define VERSION_MINOR  7
 
 
 #endif /*__MAIN_H__*/
