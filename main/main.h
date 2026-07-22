0bc4a1331bf62cace80176f575c5961fe1365d4a
bin <yu200141526@163.com>
2026-07-22 16:06:59 +0800

    V0.8

--- main/main.h
+++ main/main.h
@@ -3,9 +3,9 @@
 
 #include "global_define.h"
 
-#define VERSION        "v0.7"
+#define VERSION        "v0.8"
 #define VERSION_MAJOR  0
-#define VERSION_MINOR  7
+#define VERSION_MINOR  8
 
 
 #endif /*__MAIN_H__*/
