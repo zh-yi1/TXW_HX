31640fa12e6694e89280161d92e8f6adc6833b15
bin <yu200141526@163.com>
2026-07-24 17:52:38 +0800

    V 0.9

--- main/main.h
+++ main/main.h
@@ -3,9 +3,9 @@
 
 #include "global_define.h"
 
-#define VERSION        "v0.8"
+#define VERSION        "v0.9"
 #define VERSION_MAJOR  0
-#define VERSION_MINOR  8
+#define VERSION_MINOR  9
 
 
 #endif /*__MAIN_H__*/
