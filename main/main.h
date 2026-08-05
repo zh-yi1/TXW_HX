efddd82cbcc935e6a95facc4ec948d60edb1a945
bin <yu200141526@163.com>
2026-08-05 16:24:00 +0800

    Revert "feat:V1.1"
    This reverts commit c89edba59be8065609d3982676e949d349028f25.

--- main/main.h
+++ main/main.h
@@ -3,9 +3,9 @@
 
 #include "global_define.h"
 
-#define VERSION        "v1.2"
-#define VERSION_MAJOR  1
-#define VERSION_MINOR  1
+#define VERSION        "v0.9"
+#define VERSION_MAJOR  0
+#define VERSION_MINOR  9
 
 
 #endif /*__MAIN_H__*/
