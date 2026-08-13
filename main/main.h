d33c56845d66c074b78f9dab686eb61a28316ade
yh <upyh@foxmail.com>
2026-08-13 07:43:45 +0100

    V1.5

--- main/main.h
+++ main/main.h
@@ -3,9 +3,9 @@
 
 #include "global_define.h"
 
-#define VERSION        "v1.4"
+#define VERSION        "v1.5"
 #define VERSION_MAJOR  1
-#define VERSION_MINOR  4
+#define VERSION_MINOR  5
 
 
 #endif /*__MAIN_H__*/
