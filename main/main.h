8c00a35532b7e1e7eadf0d6d88069b54ded2a619
bin <yu200141526@163.com>
2026-09-11 16:39:39 +0800

    V2.1

--- main/main.h
+++ main/main.h
@@ -4,9 +4,9 @@
 #include "global_define.h"
 
 /* 屏幕显示软件版本 (信息页 "MCU Vx.y" / 开机 LOG / 产测): 改这里即可 */
-#define VERSION        "V2.0"
+#define VERSION        "V2.1"
 #define VERSION_MAJOR  2
-#define VERSION_MINOR  0
+#define VERSION_MINOR  1
 
 #endif /*__MAIN_H__*/
 
